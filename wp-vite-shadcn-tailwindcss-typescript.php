760d1d4bf8a8366b40035602a919f012f812bc5c
Mamunur Rashid <rmamunur105@gmail.com>
2025-08-24 17:45:37 +0600

    Create wp-vite-shadcn-tailwindcss-typescript.php

---
+++ wp-vite-shadcn-tailwindcss-typescript.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Vite shadcn tailwindcss typescript
+ * Plugin URI: #
+ * Description: Vite shadcn tailwindcss typescript
+ * Version: 1.0
+ * Author: mamunur105
+ * Text Domain: wp-vite-shadcn-tailwindcss-typescript
+ * Author URI: https://github.com/mamunur105
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+define( 'PPNINJA_VERSION', '0.0.1' );
+
+define( 'PPNINJA_FILE', __FILE__ );
+
+define( 'PPNINJA_FILE_BASENAME', plugin_basename( PPNINJA_FILE ) );
+
+define( 'PPNINJA_FILE_URL', plugins_url( '', PPNINJA_FILE ) );
+
+define( 'PPNINJA_DIR_URL', plugin_dir_url(  PPNINJA_FILE ) );
+
+define( 'PPNINJA_FILE_ABSPATH', dirname( PPNINJA_FILE ) );
+
+define( 'PPNINJA_FILE_PATH', plugin_dir_path( __FILE__ ) );
