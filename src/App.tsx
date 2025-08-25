2ab1543f40b89956916b06878699fac1fb5066cb
Mamunur Rashid <rmamunur105@gmail.com>
2025-08-25 23:06:43 +0600

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-
-
+import { Button } from "@/components/ui/button"
 
 export default function App() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-blue-100">
-        <div className="max-w-sm p-6 bg-white rounded-2xl shadow-lg text-center">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Hello, Tailwind + TypeScript!
-        </h1>
-        <p className="text-gray-600">
-        This is a simple example using <span className="font-medium">Vite</span>,
-        <span className="font-medium"> React</span>, <span className="font-medium">TypeScript</span>,
-    and <span className="font-medium">Tailwind CSS</span>.
-    </p>
-
-    </div>
-    </div>
-);
+            <div className="max-w-sm p-6 bg-white rounded-2xl shadow-lg text-center">
+                <h1 className="text-2xl font-bold text-gray-800 mb-4">
+                  Hello, Tailwind + TypeScript!
+                </h1>
+                <p className="text-gray-600">
+                    This is a simple example using <span className="font-medium">Vite</span>,
+                    <span className="font-medium"> React</span>, <span className="font-medium">TypeScript</span>,
+                and <span className="font-medium">Tailwind CSS</span>.
+                </p>
+                <Button variant="outline">Button</Button>
+            </div>
+        </div>
+    );
 }
